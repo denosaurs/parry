fe5c7aaee3f84bef5c14ee700d3127e8a40f3fa9
eliassjogreen <eliassjogreen1@gmail.com>
2019-11-23 20:25:58 +0100

    Update, works better in browser than in deno atm

--- mod.ts
+++ mod.ts
@@ -1,29 +1,65 @@
-export default function parry<T extends any[], U>(
-    original: (...params: T) => U
-): (...params: T) => Promise<U> {
-    const script = `onmessage = function (e) { if (e.data === "close") self.close(); postMessage((${original.toString()})(...e.data)); }`;
-    const blob = new Blob([script], { type: "text/javascript" });
+type AsyncFunction<S extends any[], T> = (...args: S) => Promise<T>;
+type MaybeAsyncFunction<S extends any[], T> = (...args: S) => T | Promise<T>;
 
-    return function(...args: T): Promise<U> {
-        const url = URL.createObjectURL(blob);
-        let worker = new Worker(url);
-        URL.revokeObjectURL(url);
+export default function parry<S extends any[], T>(
+    original: MaybeAsyncFunction<S, T>
+): AsyncFunction<S, T> {
+    let id = 0;
+    let promises: { [id: number]: [(value?: T | PromiseLike<T>) => void, (reason?: any) => void] } = {};
 
-        const call = (...args) => worker.postMessage(args);
-        const close = () => worker.postMessage("close");
+    const script =
+        `const _f = ${original.toString()};` +
+        `\nonmessage = function (e) {` +
+        `\n    const { type, id, data } = e.data;` +
+        `\n    switch(type) {` +
+        `\n        case "close":` +
+        `\n            self.close();` +
+        `\n            break;` +
+        `\n        case "call":` +
+        `\n            Promise.resolve(data).then(v => _f.apply(_f, v)).then(` +
+        `\n                r => postMessage({ type: "resolve", id: id, data: r }),` +
+        `\n                e => postMessage({ type: "reject", id: id, data: e })` +
+        `\n            );` +
+        `\n            break;` +
+        `\n        default:` +
+        `\n            throw "Unknown message type";` +
+        `\n    }` +
+        `\n};`;
 
-        call(args);
+    let worker = new Worker(URL.createObjectURL(new Blob([script], { type: "text/javascript" })));
 
+    worker.onmessage = e => {
+        const { type, id, data } = e.data;
+        
+        switch(type) {
+            case "resolve":
+                promises[id][0](data);
+                break;
+            case "reject":
+                promises[id][1](data);
+                break;
+            default:
+                throw "Unknown message type";
+        }
+
+        delete promises[id];
+    };
+
+    const call = (id: number, ...args: S) => {
+        worker.postMessage({
+            type: "call",
+            id: id,
+            data: args
+        });
+    };
+    const close = () => worker.postMessage({ type: "close" });
+
+    return (...args: S): Promise<T> => {
         return new Promise((resolve, reject) => {
-            worker.onmessage = msg => {
-                close();
-                resolve(msg.data);
-            };
+            promises[id] = [resolve, reject];
+            call(id, ...args);
 
-            worker.onerror = () => {
-                close();
-                reject();
-            };
+            id++;
         });
     };
 }
