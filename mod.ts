4f1f46af5f4d43c0cbc9d597e8eac2a082dd1aae
eliassjogreen <eliassjogreen1@gmail.com>
2019-11-23 02:27:18 +0100

    Worker now closes when done. Deno really needs to implement worker.terminate

--- mod.ts
+++ mod.ts
@@ -1,18 +1,27 @@
-export default function parry<T extends any[], U>(original: (...params: T) => U): (...params: T) => Promise<U> {
-    return function (...args: T): Promise<U> {
+export default function parry<T extends any[], U>(
+    original: (...params: T) => U
+): (...params: T) => Promise<U> {
+    return function(...args: T): Promise<U> {
         return new Promise((resolve, reject) => {
-            const script = `onmessage = function (e) { postMessage((${original.toString()})(...e.data)); }`;
+            const script = `onmessage = function (e) { if (e.data === "close") self.close(); postMessage((${original.toString()})(...e.data)); }`;
 
             const blob = new Blob([script], { type: "text/javascript" });
             const url = URL.createObjectURL(blob);
-    
+
             let worker = new Worker(url);
             URL.revokeObjectURL(url);
-    
-            worker.onmessage = msg => resolve(msg.data);
-            worker.onerror = () => reject();
+
+            worker.onmessage = msg => {
+                worker.postMessage("close")
+                resolve(msg.data);
+            };
+            
+            worker.onerror = () => {
+                worker.postMessage("close")
+                reject();
+            };
 
             worker.postMessage(args);
         });
-    }
+    };
 }
