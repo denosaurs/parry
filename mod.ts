7253314f667e450daa2e5508234d403cfd6669fd
eliassjogreen <eliassjogreen1@gmail.com>
2019-11-23 14:04:44 +0100

    Cleaned up a bit

--- mod.ts
+++ mod.ts
@@ -1,27 +1,29 @@
 export default function parry<T extends any[], U>(
     original: (...params: T) => U
 ): (...params: T) => Promise<U> {
+    const script = `onmessage = function (e) { if (e.data === "close") self.close(); postMessage((${original.toString()})(...e.data)); }`;
+    const blob = new Blob([script], { type: "text/javascript" });
+
     return function(...args: T): Promise<U> {
+        const url = URL.createObjectURL(blob);
+        let worker = new Worker(url);
+        URL.revokeObjectURL(url);
+
+        const call = (...args) => worker.postMessage(args);
+        const close = () => worker.postMessage("close");
+
+        call(args);
+
         return new Promise((resolve, reject) => {
-            const script = `onmessage = function (e) { if (e.data === "close") self.close(); postMessage((${original.toString()})(...e.data)); }`;
-
-            const blob = new Blob([script], { type: "text/javascript" });
-            const url = URL.createObjectURL(blob);
-
-            let worker = new Worker(url);
-            URL.revokeObjectURL(url);
-
             worker.onmessage = msg => {
-                worker.postMessage("close")
+                close();
                 resolve(msg.data);
             };
-            
+
             worker.onerror = () => {
-                worker.postMessage("close")
+                close();
                 reject();
             };
-
-            worker.postMessage(args);
         });
     };
 }
